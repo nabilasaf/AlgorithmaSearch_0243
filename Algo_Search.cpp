fe456d5d1daa0be77c08992ef66d4fda03096ac0
nabilasaf <nabilasafitri861@gmail.com>
2024-04-04 14:59:17 +0700

    menampilkan data

--- Algo_Search.cpp
+++ Algo_Search.cpp
@@ -5,6 +5,18 @@ int arr[20]; // Array to be searched
 int n;       // Number of elements in the array
 int i;       // Index of array element
 
+void input()
+{
+    while(true)
+    {
+        cout << "Enter the number of element in the array : ";
+        cin >> n;
+        if ((n > 0)&& (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
+}
 int main() {
 
 }
