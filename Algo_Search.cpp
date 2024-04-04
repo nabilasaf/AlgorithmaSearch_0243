305d7b81898185e46518a4fbff3d009b778c61b0
nabilasaf <nabilasafitri861@gmail.com>
2024-04-04 15:16:19 +0700

    step_6 (menampilkan ketika array i = item)

--- Algo_Search.cpp
+++ Algo_Search.cpp
@@ -45,6 +45,10 @@ void LinearSearch()
         {
             ctr++;
             if (arr[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; //step 6
+                break;
+            }
         }
     }
 }
