969b16be22adcd0e42ee4d8910eb9829b68b8860
nabilasaf <nabilasafitri861@gmail.com>
2024-04-04 15:10:48 +0700

    step_1

--- Algo_Search.cpp
+++ Algo_Search.cpp
@@ -36,6 +36,8 @@ void LinearSearch()
     do
     {
         // Accept the number to be searched
+        cout << "\nEnter the element you want to search : "; //step 1
+        cin >> item;
     }
 }
 int main() {
