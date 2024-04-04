9e8a88de90c80e586df6f0cdaccfec86a7d2d872
nabilasaf <nabilasafitri861@gmail.com>
2024-04-04 15:20:40 +0700

    menampilkan step_5

--- Algo_Search.cpp
+++ Algo_Search.cpp
@@ -52,6 +52,10 @@ void LinearSearch()
             i++; // step 4
         }
         if (i == n) // step 5
+        cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons: " << ctr << endl;
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
     }
 }
 int main() {
